8eca5bf33c3261fe7f08391e1e07df9155050819
Simon Rupf <simon@rupf.net>
2023-06-22 17:58:38 +0200

    catch errors, return these on STDERR and exit non-zero

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 extern crate serde_json;
 extern crate serde_yaml;
 use std::io::{stdin, stdout};
+use std::process::exit;
 
 fn main() {
-    let value: serde_json::Value = serde_yaml::from_reader(stdin()).unwrap();
-    serde_json::to_writer(stdout(), &value).unwrap();
+    let value: serde_json::Value = match serde_yaml::from_reader(stdin()) {
+        Ok(parsed_yaml) => parsed_yaml,
+        Err(e) => {
+            eprintln!("Error parsing input: {e:?}");
+            exit(1);
+        }
+    };
+    if let Err(e) = serde_json::to_writer(stdout(), &value) {
+        eprintln!("Error serializing output: {e:?}");
+        exit(2);
+    }
 }
