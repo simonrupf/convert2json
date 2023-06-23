6cd851f7a410d9813f04c40fd810a5ac14e4f9c9
Simon Rupf <simon@rupf.net>
2023-06-23 12:45:40 +0200

    nicer errors

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let value: serde_json::Value = match serde_yaml::from_reader(stdin()) {
         Ok(parsed_yaml) => parsed_yaml,
         Err(e) => {
-            eprintln!("Error parsing input: {e:?}");
+            eprintln!("Error parsing input: {0}", e.to_string());
             exit(1);
         }
     };
     if let Err(e) = serde_json::to_writer(stdout(), &value) {
-        eprintln!("Error serializing output: {e:?}");
+        eprintln!("Error serializing output: {0}", e.to_string());
         exit(2);
     }
 }
